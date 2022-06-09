9c01d5c1217f1ed7367d8fa6212b15cbf5b04c00
Scott Lin <scott.lin@tw.synaptics.com>
2022-06-09 02:16:07 -0700

    Set textTransform to none for MUI buttons

--- src/ui/mui_theme.ts
+++ src/ui/mui_theme.ts
@@ -1,46 +1,46 @@
 const webdsTheme = {
   palette: {
     primary: {
-      light: '#5aacf6',
-      main: '#007dc3',
-      dark: '#005192',
-      contrastText: '#fff'
+      light: "#5aacf6",
+      main: "#007dc3",
+      dark: "#005192",
+      contrastText: "#fff"
     }
   },
 
   typography: {
-    fontFamily: [
-      'Arial',
-      'Roboto',
-      'Helvetica',
-      'sans-serif'
-    ].join(',')
+    fontFamily: ["Arial", "Roboto", "Helvetica", "sans-serif"].join(",")
   },
 
   components: {
     MuiAvatar: {
       defaultProps: {
-        sx: {bgcolor: '#007dc3'}
+        sx: { bgcolor: "#007dc3" }
       }
     },
     MuiButton: {
       defaultProps: {
-        variant: 'contained'
+        variant: "contained"
+      },
+      styleOverrides: {
+        root: {
+          textTransform: "none"
+        }
       }
     },
     MuiFab: {
       defaultProps: {
-        color: 'primary',
-        size: 'small'
+        color: "primary",
+        size: "small"
       }
     },
     MuiTypography: {
       defaultProps: {
-        color: 'textPrimary',
-        component: 'div'
+        color: "textPrimary",
+        component: "div"
       }
     }
   }
 };
 
-export default webdsTheme
+export default webdsTheme;

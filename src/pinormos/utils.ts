d841dc038cd21cd787a0600c765bdec36121a6eb
Scott Lin <scott.lin@tw.synaptics.com>
2023-07-20 11:53:30 +0800

    Add setting of date and time

--- src/pinormos/utils.ts
+++ src/pinormos/utils.ts
@@ -465,6 +465,35 @@ export const pollConnection = async () => {
   setTimeout(pollConnection, pollConnectionPeriod);
 };
 
+const setDate = async (date: string) => {
+  let token: string;
+  let dataToSend: any = {
+    command: {
+      action: 'setDate',
+      target: date
+    }
+  };
+  try {
+    token = await requestAPI<any>('general', {
+      body: JSON.stringify(dataToSend),
+      method: 'POST'
+    });
+  } catch (error) {
+    console.error(`Error - POST /webds/general\n${dataToSend}\n${error}`);
+    return Promise.reject('Failed to set date');
+  }
+  dataToSend.command.token = token;
+  try {
+    await requestAPI<any>('general', {
+      body: JSON.stringify(dataToSend),
+      method: 'POST'
+    });
+  } catch (error) {
+    console.error(`Error - POST /webds/general\n${dataToSend}\n${error}`);
+    return Promise.reject('Failed to set date');
+  }
+};
+
 export const updateDSDKInfo = async () => {
   try {
     await requestAPI<any>('general');
@@ -513,6 +542,20 @@ export const updateDSDKInfo = async () => {
   }
 
   await checkConnection();
+
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+  const day = String(currentDate.getDate()).padStart(2, '0');
+  const hours = String(currentDate.getHours()).padStart(2, '0');
+  const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+  const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+  const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  try {
+    await setDate(formattedDateTime);
+  } catch (error) {
+    console.error(error);
+  }
 
   return Promise.resolve();
 };
